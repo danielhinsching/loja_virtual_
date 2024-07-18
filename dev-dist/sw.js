41380c1a6646a177cb68bbcd1a57ec916d85d349
danielhinsching <danielsantoshinsching@gmail.com>
2024-07-18 13:18:31 -0300

    Criação Camada de armazenamento.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1gt6s59uf7g"
+    "revision": "0.2pmc3usejbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
