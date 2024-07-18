dd13afa7bad00b1dd4739bcb45e83719325ac295
danielhinsching <danielsantoshinsching@gmail.com>
2024-07-17 21:47:23 -0300

    Criação da camada de serviços; Conclusão tela de login.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="Y3n0LHyQ1tBwJTh7ROCOmXcv"
+    ></passage-auth>
+  </div>
 </template>
+
+<style></style>
